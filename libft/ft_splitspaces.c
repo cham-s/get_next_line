e6b1a9e7e2c06475c6f8877459d55acadac5f42c
Chamsidine ATTOUMANI <cattouma@e1r9p19.42.fr>
2016-01-25 18:55:52 +0100

    added libft

---
+++ libft/ft_splitspaces.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_splitspaces.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cattouma <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/25 18:41:46 by cattouma          #+#    #+#             */
+/*   Updated: 2016/01/25 18:43:26 by cattouma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static size_t	to_allocate(const char *s)
+{
+	size_t	size;
+
+	size = 0;
+	while (ft_isspace(*s) && *s)
+		s++;
+	while (*s)
+	{
+		while (ft_isspace(*s))
+			s++;
+		if ((*s && (!ft_isspace(*s))) &&
+				((ft_isspace(*(s + 1))) || *(s + 1) == '\0'))
+			size++;
+		if (*s)
+			s++;
+	}
+	return (size);
+}
+
+static size_t	ft_strlenchar(const char *s)
+{
+	size_t i;
+
+	i = 0;
+	while (ft_isspace(*s) && *s)
+		s++;
+	while (!ft_isspace(*s) && *s)
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
+
+char			**ft_splitspaces(char const *s)
+{
+	char	**new;
+	size_t	len;
+	size_t	i;
+	size_t	strlen;
+
+	if (s == NULL)
+		return (NULL);
+	i = 0;
+	strlen = 0;
+	len = to_allocate(s);
+	new = (char **)ft_memalloc(len + 1);
+	if (!new)
+		return (NULL);
+	while (len--)
+	{
+		while (ft_isspace(*s))
+			s++;
+		strlen = ft_strlenchar(s);
+		new[i] = ft_strnew(strlen);
+		ft_memcpy(new[i], s, strlen);
+		s += strlen;
+		i++;
+	}
+	new[i] = NULL;
+	return (new);
+}
